afd9b379b3dbd6dc4bcd835158fb7370502b4ec7
bennyt88 <98079345+bennyt88@users.noreply.github.com>
2023-02-06 14:33:47 +1100

    Update quiz4.sql

--- quiz4.sql
+++ quiz4.sql
@@ -1,1 +1,27 @@
+CREATE or REPLACE FUNCTION 
+	Q11(partial_title text) RETURNS setof text
+AS $$
+	DECLARE
+		_m Movies;
+		_partial_title ilike %partial_title%;
+	BEGIN
+			select 	m.* 
+			from 	movies m
+					join principals pr on m.id=pr.movie_id
+			where 	m.title ilike _partial_title and pr.role not like 'producer'
+			group by m.id
+			order by m.title ASC
+		loop
+			return next ''|| _m.title||' has ';-- || ||' cast and crew';
+		end loop;
+		
+		if(not found) then
+			return next 'No matching titles';
+		end if;
 
+	END;
+	
+$$ LANGUAGE plpgsql;
+
+
+select * from Q11('Fight Club');
